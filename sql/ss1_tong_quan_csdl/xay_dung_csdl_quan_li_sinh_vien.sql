a2226460b7def4195daab2e855d99a8e8924c5af
Chung128 <tranvanchung120897@gmail.com>
2025-05-19 15:22:19 +0700

    bai tap

--- sql/ss1_tong_quan_csdl/xay_dung_csdl_quan_li_sinh_vien.sql
+++ sql/ss1_tong_quan_csdl/xay_dung_csdl_quan_li_sinh_vien.sql
@@ -1,10 +1,10 @@
 create database student_management;
 use student_management;
-create table Class(
-id int,
+create table class(
+id int primary key,
 `name` varchar(100)
 );
-create table Teacher(
+create table teacher(
 id int,
 `name` varchar(100),
 age int,
@@ -13,3 +13,10 @@ country varchar(100)
 select * from Class;
 select id from Teacher;
 select * from Teacher;
+insert into class(id,name) values(6,'bình'),(5,'Khánh');
+update class set name='thẻo lê' where id=5;
+delete from class where id=6;
+-- drop table class;
+-- drop table Teacher;
+-- drop database student_management;
+-- drop database class;
